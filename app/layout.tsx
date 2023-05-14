025a408489ce77359791dfb879cca651a849b011
Lloyd P Babu <lloydpbabu@outlook.com>
2023-05-14 22:16:13 +0530

    Feat: Add UserMenu List items

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Nunito} from "next/font/google";
 import './globals.css'
-import Navbar from "./component/navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 
 
 export const metadata = {
